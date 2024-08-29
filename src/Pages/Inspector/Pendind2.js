a54c31724d41140d683c464c225003ea6e835b1b
ishraq_samiha <148335843+isssh23@users.noreply.github.com>
2024-08-29 15:43:21 +0600

    Create Pendind2.js

---
+++ src/Pages/Inspector/Pendind2.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from '../../Components/Navbar/Navbar';
+import Pending_2 from '../../Components/Inspector/Pending_2';
+
+const Pending2 = () => {
+    return (
+        <div>
+            <Navbar />
+            <Pending_2 />
+        </div>
+    );
+};
+
+export default Pending2;
