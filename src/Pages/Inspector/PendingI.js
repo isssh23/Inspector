c55aa06f5572d24c9f9e23fe5e56ee9e750cf127
ishraq_samiha <148335843+isssh23@users.noreply.github.com>
2024-08-29 15:44:21 +0600

    Create PendingI.js

---
+++ src/Pages/Inspector/PendingI.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from '../../Components/Navbar/Navbar';
+import Pending_I from '../../Components/Inspector/Pending_I';
+
+const PendingI = () => {
+    return (
+        <div>
+            <Navbar />
+            <Pending_I />
+        </div>
+    );
+};
+
+export default PendingI;
